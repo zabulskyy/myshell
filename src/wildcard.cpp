733e39b5fdda3b6c2112cf082b4f0c90bb95ee85
irazakharchenko <ira99.zakharchenko@gmail.com>
2018-03-19 21:55:21 +0200

    small fixes, add wildcard

---
+++ src/wildcard.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+#include <vector>
+#include <wait.h>
+#include <sstream>
+#include <algorithm>
+#include <dirent.h>
+
+
+using namespace std;
+char cur_dir[1024] = "/home/iryna/CLionProjects/myshell/src";
+
+int merrno = 0;
+vector<string> wildcards(string str);
+vector<string> wildcards(string str){
+    // *.txt, ab??.dat, xy[klm].sh
+    DIR *dir;
+    vector<string> res;
+    struct dirent *ent;
+    size_t found = str.find_last_of("/\\");
+    string dir_open ;
+    if (found != string::npos){
+        dir_open = str.substr(found+1);
+    } else
+        dir_open = cur_dir;
+    if (   (dir = reinterpret_cast<DIR *>(opendir ((dir_open).c_str()) != NULL))) {
+        /* print all the files and directories within directory */
+        while ((ent = readdir (dir)) != NULL) {
+            printf ("%s\n", ent->d_name);
+        }
+        closedir (dir);
+    } else {
+        /* could not open directory */
+        merrno = 2;
+    }
+    res.emplace_back("hi");
+    return res;
+}
+
+int main(){
+    wildcards("jrjrjr");
+    return 0;
+}
